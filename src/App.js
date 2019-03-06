52a01950ab168087596f6f157568996d0c28bb13
DanielHammami <daniel.hammami@yahoo.com>
2019-03-06 17:49:47 +0100

    Setup
    Setup sass
    
    Add scss file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './resources/styles.css'
+import './scss/styles.scss'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    )
+  }
+}
