cc88c8d964992cd294ac40c2297ac8c90ad893a2
DanielHammami <daniel.hammami@yahoo.com>
2019-03-06 18:01:38 +0100

    Add scss file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './resources/styles.css'
+import './scss/styles.scss'
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        Hello 
+        Hello
       </div>
     )
   }
